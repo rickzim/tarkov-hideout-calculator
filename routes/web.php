ceb4956dcdea99bb9ae7d0a4f89dc6c74dbc8add
Rick Zimmermans (work) <rickzimmermans@gmail.com>
2024-01-26 11:48:13 +0100

    added redirect to login

--- routes/web.php
+++ routes/web.php
@@ -16,7 +16,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return redirect('/admin/login');
 });
 
 Route::get('/tinker', function () {
